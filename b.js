88cfd26f2460d394a740e4dcdbcc102780b07987
jibril-258 <azjibmika@gmail.com>
2024-11-03 14:50:17 +0100

    Add files via upload

---
+++ b.js
@@ -0,0 +1,12 @@
+function ouvrirPage(){
+    var a = document.getElementById("search").value;
+    if(a === "123456789a"){
+ window.open()
+    }
+    if(a === ""){
+        window.alert("casse toi")
+           }
+           if(a === "o"){
+            window.alert("casse toi")
+               }
+}
